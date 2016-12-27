c394aad4b1b8d64b1aa7d628623c861810256ebb
oswystan <oswystan@126.com>
2016-12-27 17:17:08 +0800

    feature: make it can login
    Signed-off-by: oswystan <oswystan@126.com>

--- iochat.js
+++ iochat.js
@@ -14,20 +14,61 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+var clients = {};
+
 function main() {
     server.listen(8000);
 
     app.get('/', function (req, res) {
         res.sendFile(__dirname + '/index.html');
     });
+    app.get('/libs/:file', function (req, res) {
+        res.sendFile(__dirname + '/libs/' + req.params["file"]);
+    });
 
     io.on('connection', function (socket) {
-        console.log("new connection");
+
+        function refreshClients() {
+            var users = [];
+            for (var v in clients) {
+                users.push(v);
+            }
+
+            io.emit("users", users);
+        }
+
         socket.on("disconnect", function (data) {
-            console.log("disconnect");
+            if (socket.nick) {
+                console.log("delete " + socket.nick);
+                delete clients[socket.nick];
+                delete socket.nick;
+            }
         });
-        socket.on("login", function (data) {
-            console.log("login : " + data);
+        socket.on("login", function (data, cb) {
+            if (socket.nick) {
+                cb(false);
+                console.log("already login by " + socket.nick);
+                return;
+            }
+            if (!(data in clients)) {
+                clients[data] = socket;
+                socket.nick = data;
+                refreshClients();
+            }
+            cb(true);
+        });
+
+        socket.on("message", function (data) {
+            io.emit("message", data);
+        });
+
+        socket.on("message_to", function (data, cb) {
+            if (data.user in clients) {
+                cb(true);
+                clients[data.user].emit("message", data.message);
+            } else {
+                cb(false);
+            }
         });
     });
 
